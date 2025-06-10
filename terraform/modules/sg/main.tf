0ce67e5107b9e6f27f24fcb9c0cfc8b288a7f122
grond <drowqueen@gmail.com>
2025-06-10 22:22:09 +0300

    Module detectio logic fix

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -1,7 +1,7 @@
 module "security-group" {
   source                   = "terraform-aws-modules/security-group/aws"
   name                     = var.name
-  use_name_prefix          = true
+  use_name_prefix          = var.use_name_prefix
   vpc_id                   = var.vpc_id
   ingress_cidr_blocks      = var.ingress_cidr_blocks
   ingress_with_cidr_blocks = var.ingress_with_cidr_blocks
